c6afc9d098ace56e9d1c44de7da8fec5985e376f
Evan Barton <notrabe1011@gmail.com>
2020-12-18 13:07:06 -0500

    having issues with tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,29 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+const user = {username: 'username', password: 'password'}
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+describe('server.js', () => {
+  describe('get jokes', () => {
+    it('should be unauthorized, return 401', async () => {
+      const res = await request(server).get('/api/jokes')
+      expect(res.status).toBe(401)
+    })
+  })
+  describe('register', () => {
+    it('status code 201', async () => {
+      await db('users').truncate()
+      const res = await request(server)
+        .post('api/auth/register')
+        .send(user)
+      expect(res.status).toBe(201)
+    })
+    
+  })
 })
